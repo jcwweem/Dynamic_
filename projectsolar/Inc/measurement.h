275e0de21026efa37137f92ebd0d43b2f2182903
jcwweem <jcwweem@avans.nl>
2018-04-12 11:55:41 +0200

    added GPIO, ADC, DAC, system clock and measurement code

--- projectsolar/Inc/measurement.h
+++ projectsolar/Inc/measurement.h
@@ -27,6 +27,7 @@ void initMeasurement(void);
 /*
  * starts the measurement of all the cells
  */
+void startMeasurement(uint16_t);
 
 // 		Private functions
 /*
@@ -35,15 +36,25 @@ void initMeasurement(void);
 uint16_t measureCel(uint16_t celNr);
 
 /*
+ *	returns the number of cells in the solar array
+ */
+uint16_t getNrOfCells();
+
+/*
+ * measures the number of cells in an array
+ */
+uint16_t measureNrOfCells();
+
+/*
  * measures the PP at a give voltage by reading the ADC for voltage and current
  * @param voltage
  */
-uint16_t measurePoint(uint32_t voltage);
+uint16_t measurePoint(uint16_t voltage);
 
 /*
- * measures the string
+ * measures the entire string
  */
-uint16_t measureString();
+uint16_t measureString(uint16_t);
 
 /*
  * This function returns the voltage that was needed for the previous MPP for a given cel
@@ -51,5 +62,10 @@ uint16_t measureString();
  */
 uint16_t* getPrevMPPVoltage(uint16_t celNr); //gets the voltage that was needed for the previous MPP for a given c
 
+/*
+ * sends the measured data to the sorting system
+ * @param de resulaten.
+ */
+void passMeasurement(uint16_t* results);
 
 #endif
--- projectsolar/Src/dac.c
+++ projectsolar/Src/dac.c
@@ -29,8 +29,6 @@ void DAC_initialiseDAC(){
 	{
 		while(1);
 	}
-
-	HAL_DAC_Start(&hdac1, DAC_CHANNEL_1);
 }
 
 void DAC_setDAC(uint32_t voltage){
