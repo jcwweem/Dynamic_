1786cf2131b47989e0fa19115f019a1d579ef567
joris pennings <jjfpenni@avans.nl>
2018-05-18 11:21:21 +0200

    update refacter/comments

--- Inc/shiftregister.h
+++ Inc/shiftregister.h
@@ -9,15 +9,12 @@
 #define SHIFTREGISTER_H_
 
 void initShiftregister();
-
-void shift_writeCellsTest(int waarde);
-
+void shift_writeCellsTest(int value);
 void measureAllCells(int* array);
-
 void shift_measureAt(int index);
-void shift1Cell(int waarde);
+void shift1Cell(int value);
 void shift1Bit(int bit);
-
 void initAmountOfCells();
+void delay();
 
 #endif /* SHIFTREGISTER_H_ */
